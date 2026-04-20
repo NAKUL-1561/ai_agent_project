7c3655a2ea7a7ec020e4ca8260307cb5577c5211
NAKUL-1561 <nakulchauhankhod@gmail.com>
2026-04-20 14:22:30 +0000

    New project: claude code using gemini2.5 flash(gemini api)

---
+++ main.py
@@ -0,0 +1,15 @@
+import os
+from google import genai
+from dotenv import load_dotenv
+
+load_dotenv()
+api_key = os.environ.get("GEMINI_API_KEY")
+client = genai.Client(api_key=api_key)
+
+if api_key is None:
+    raise RuntimeError("environment variable wasn't found")
+
+response = client.models.generate_content(
+    model="gemini-2.5-flash", contents="Why is Boot.dev such a great place to learn backend development? Use one paragraph maximum."
+)
+print(response.text)
