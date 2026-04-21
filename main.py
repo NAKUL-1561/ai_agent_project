6fef3c81b3e0d10327e6e803695610166f21c02b
NAKUL-1561 <nakulchauhankhod@gmail.com>
2026-04-21 07:40:36 +0000

    Updated the input from the user we can now parse it through command line and pass in the multiple messages

--- main.py
+++ main.py
@@ -1,19 +1,25 @@
+import argparse
 import os
 from google import genai
+from google.genai import types
 from dotenv import load_dotenv
 
 load_dotenv()
 api_key = os.environ.get("GEMINI_API_KEY")
 client = genai.Client(api_key=api_key)
 
-user_prompt = "Why is Boot.dev such a great place to learn backend development? Use one paragraph maximum."
+parser = argparse.ArgumentParser(description="Chatbot")
+parser.add_argument("user_prompt", type=str, help="User prompt")
+args = parser.parse_args()
+user_prompt = args.user_prompt
 
+messages = [types.Content(role="user", parts=[types.Part(text=args.user_prompt)])]
 
 if api_key is None:
     raise RuntimeError("environment variable wasn't found")
 
 response = client.models.generate_content(
-    model="gemini-2.5-flash", contents=user_prompt
+    model="gemini-2.5-flash", contents=messages
 )
 if response.usage_metadata is None:
     raise RuntimeError("API response missing  usage metadata. Request may have failed.")
