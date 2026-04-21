294107bbc62b930d6055e6c0289764192f8866da
NAKUL-1561 <nakulchauhankhod@gmail.com>
2026-04-21 11:53:22 +0000

    Added --verbose flag to toggle through verbose output

--- main.py
+++ main.py
@@ -9,11 +9,12 @@ api_key = os.environ.get("GEMINI_API_KEY")
 client = genai.Client(api_key=api_key)
 
 parser = argparse.ArgumentParser(description="Chatbot")
-parser.add_argument("user_prompt", type=str, help="User prompt")
+parser.add_argument("prompt", help="The prompt to send to the model")
+parser.add_argument("--verbose", action="store_true", help="Enable verbose output")
 args = parser.parse_args()
-user_prompt = args.user_prompt
+user_prompt = args.prompt
 
-messages = [types.Content(role="user", parts=[types.Part(text=args.user_prompt)])]
+messages = [types.Content(role="user", parts=[types.Part(text=user_prompt)])]
 
 if api_key is None:
     raise RuntimeError("environment variable wasn't found")
@@ -27,8 +28,9 @@ if response.usage_metadata is None:
 prompt_tokens = response.usage_metadata.prompt_token_count
 response_tokens = response.usage_metadata.candidates_token_count
 
-print(f"User Prompt: {user_prompt}")
-print(f"Prompt tokens: {prompt_tokens}")
-print(f"Response tokens: {response_tokens}")
-print("Response")
+if args.verbose:
+    print(f"User prompt: {user_prompt}")
+    print(f"Prompt tokens: {prompt_tokens}")
+    print(f"Response tokens: {response_tokens}")
+
 print(response.text)
