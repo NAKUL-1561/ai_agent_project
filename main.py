c3c87097d102d740a0cbbcf85f7403ebeaf6067a
NAKUL-1561 <nakulchauhankhod@gmail.com>
2026-05-04 04:22:34 +0000

    System prompt: a special prompt that goes at the beginning of the conversation that carries more weight than a typical user prompt

--- main.py
+++ main.py
@@ -1,6 +1,7 @@
 import argparse
 import os
 from google import genai
+from prompt import system_prompt
 from google.genai import types
 from dotenv import load_dotenv
 
@@ -20,7 +21,11 @@ if api_key is None:
     raise RuntimeError("environment variable wasn't found")
 
 response = client.models.generate_content(
-    model="gemini-2.5-flash", contents=messages
+    model = "gemini-2.5-flash", 
+    contents = messages, 
+    config = types.GenerateContentConfig(
+    system_instruction=system_prompt, 
+    temperature=0)
 )
 if response.usage_metadata is None:
     raise RuntimeError("API response missing  usage metadata. Request may have failed.")
